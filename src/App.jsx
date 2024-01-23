696c3fe638b332d56d6e97979f9582ccea1b31c0
Gabriel Torres <gabrieltorresp2009@gmail.com>
2024-01-23 17:54:09 -0300

    Add the main components and logo icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+
+import Home from "./components/Home";
+import Nav from "./components/nav";
+import About from "./components/About";
+import Benefits from "./components/Benefits";
+import Service from "./components/Service";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <h1 className='text-[5rem] text-center font-bold text-hunterGreen'>Nuttri</h1>;
+  return (
+    <>
+      <Nav />
+      <Home />
+      <About />
+      <Benefits/>
+      <Service/>
+      <Pricing/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </>
+  );
 };
 
 export default App;
