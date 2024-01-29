77959f1ec86cc6c4e2e08ce7a6282294abba7f2f
João Vitor <devjohncb@gmail.com>
2024-01-29 18:56:43 -0300

    Footer component created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
-
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-full p-[2rem_0rem] flex flex-col gap-y-4 md:flex-row items-center justify-around">
+      <img
+        src="/greenLogo.png"
+        alt=""
+        className="w-[50px] sm:w-[70px] md:w-[100px]"
+      />
+      <nav className="flex gap-x-3">
+        <a href="" className={'text-[1rem] sm:text-[20px] font-medium'}>
+          Início
+        </a>
+        <a href="" className={'text-[1rem] sm:text-[20px] font-medium'}>
+          Sobre
+        </a>
+        <a href="" className={'text-[1rem] sm:text-[20px] font-medium'}>
+          Benefícios
+        </a>
+        <a href="" className={'text-[1rem] sm:text-[20px] font-medium'}>
+          Planos
+        </a>
+        <a href="" className={'text-[1rem] sm:text-[20px] font-medium'}>
+          Contato
+        </a>
+      </nav>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
